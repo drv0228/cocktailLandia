76fddc1eefd41c8fd99f1476b9eb6634c1d2aebf
drv0228 <110354201+drv0228@users.noreply.github.com>
2023-12-25 21:21:23 -0500

    Implement routing and NotFoundPage page
    - Create routes for different app sections
    - Introduce NotFoundPage for handling 404 errors

---
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,12 @@
+function NotFoundPage() {
+    return (
+      <>
+        <h1 className="error-404">404 error</h1>
+        <p className="error-404">
+          Sorry! No product with that description exists in our store!
+        </p>
+      </>
+    );
+  }
+  
+  export default NotFoundPage;
