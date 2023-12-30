969ee36c45453fd4ab9de0122394b35ab1bca45c
drv0228 <110354201+drv0228@users.noreply.github.com>
2023-12-30 00:40:14 -0500

    Implemented search functionality with backend API integration
    - Create a responsive search bar component
    - Configure frontend to fetch and display data from backend API
    - Finalize design with polished styling, including animations

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -8,19 +8,20 @@ import CocktailsList from "../../components/CocktailsList/CocktailsList";
 const HomePage = ({ onSearch, refresh, cocktails }) => {
   return (
     <main>
+      <div className="home-page__navbar">
       <div className="logo__container">
-     {/* <Link to="/" className="page__link" onClick={refresh}> */}
+     <Link to="/" className="page__link" onClick={refresh}>
           <img
             className="logo__image"
             src={logo}
             alt={`Logo that represent the company website`}
           />
-        {/* </Link> */}
-        <p className="logo__title">cocktailLandia</p>
-        <SearchBar onSearch={onSearch} />
-        </div>
-      <CocktailLandiaList cocktails={cocktails ?? []} refresh={refresh} />
-      <CocktailsList />
+        </Link>
+        <p className="logo__title">cocktailLandia</p></div>
+        <SearchBar onSearch={onSearch} /></div>
+        
+        <CocktailLandiaList cocktails={cocktails ?? []} refresh={refresh} />
+        <CocktailsList />
     </main>
   );
 };
